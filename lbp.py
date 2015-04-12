f903d9c330e169a98ef5546ada9baafb5e206b77
Tim van der Meij <timvandermeij@gmail.com>
2015-04-12 16:44:57 +0200

    Improved modularity and start of multiprocessing part

--- lbp.py
+++ lbp.py
@@ -5,48 +5,61 @@ from PIL import Image
 
 class LBP:
     def __init__(self, filename):
-        self.filename = filename
+        self.image = Image.open(filename)
+        self.width = self.image.size[0]
+        self.height = self.image.size[1]
+        self.pixels = list(self.image.getdata())
+        self.patterns = []
 
-    def process(self):
-        # Open the image and convert to grayscale
-        image = Image.open(self.filename).convert('L')
+    def execute(self):
+        self._preprocess()
+        self._process()
+        self._output()
+
+    def _preprocess(self):
+        # Convert the image to grayscale
+        self.image = self.image.convert('L')
 
         # Make pixels accessible like a 2D array (list of lists)
-        pixels = list(image.getdata())
-        width, height = image.size
-        pixels = [pixels[i * width:(i + 1) * width] for i in xrange(height)]
+        self.pixels = [self.pixels[i * self.width:(i + 1) * self.width] for i in xrange(self.height)]
 
+    def _process(self):
         # Calculate LBP for each non-edge pixel
-        results = []
-        for i in xrange(1, height - 1):
-            for j in xrange(1, width - 1):
-                pixel = pixels[i][j]
+        neighbors = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
+        for i in xrange(1, self.height - 1):
+            for j in xrange(1, self.width - 1):
+                pixel = self.pixels[i][j]
 
                 # Compare this pixel to its neighbors, starting at the top-left
                 # pixel and moving clockwise, and use bit operations to quickly
                 # update the feature vector
-                result = 0
-                neighbors = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+                pattern = 0
                 for index in xrange(len(neighbors)):
                     neighbor = neighbors[index]
-                    if pixel > pixels[i + neighbor[0]][j + neighbor[1]]:
-                        result = result | (1 << index)
+                    if pixel > self.pixels[i + neighbor[0]][j + neighbor[1]]:
+                        pattern = pattern | (1 << index)
 
-                results.append(result)
+                self.patterns.append(pattern)
 
+    def _output(self):
         # Write the result to an image file
-        result_image = Image.new(image.mode, (width - 2, height - 2))
-        result_image.putdata(results)
+        result_image = Image.new(self.image.mode, (self.width - 2, self.height - 2))
+        result_image.putdata(self.patterns)
         result_image.save("output.png")
 
 def main(argv):
     filename = argv[0] if len(argv) > 0 else "input.png"
+    num_processes = int(argv[1]) if len(argv) > 1 else 1
    
     if os.path.isfile(filename):
-        lbp = LBP(filename)
-        lbp.process()
+        if num_processes == 1:
+            lbp = LBP(filename)
+        else:
+            lbp = Multiprocessing_LBP(filename, num_processes)
+
+        lbp.execute()
     else:
-        print("The file " + filename + " does not exist.")
+        print("File " + filename + " does not exist.")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
