94c8c0a2ba2fc612e963091242b0c59effbaeea1
Tim van der Meij <timvandermeij@gmail.com>
2015-04-27 00:48:46 +0200

    Implemented benchmark runner
    Supersedes the old, simple benchmarking shell script. This benchmark
    runner implemented in Python does the same as the shell script (calling
    /usr/bin/time, etc.), but adds the ability to test all algorithms for
    all number of cores. On top of that it writes the results to a clean
    JSON file, reading for plotting.

---
+++ benchmark.py
@@ -0,0 +1,73 @@
+# TODO:
+# - Round long numbers to two decimals
+# - Investigate how to interpret time
+# - SVG plot
+# - Possibility to run other images
+
+import sys
+import multiprocessing
+import subprocess
+import json
+
+class Results:
+    def __init__(self):
+        self.results = {
+            'lbp': [],
+            'multi-lbp': [],
+            'multi-split-lbp': []
+        }
+
+    def get(self):
+        return self.results
+
+    def append(self, category, cores, output):
+        result = self._parse(output, cores)
+        self.results[category].append(result)
+
+    def _parse(self, output, cores):
+        result = {
+            'cores': cores,
+            'time': 0,
+            'memory': 0
+        }
+        while True:
+            line = output.readline()
+            if line == "":
+                break
+            if line.find('User time (seconds)') != -1:
+                result['time'] += float(line.split(':')[1])
+            if line.find('System time (seconds)') != -1:
+                result['time'] += float(line.split(':')[1])
+            if line.find('Maximum resident set size (kbytes)') != -1:
+                # Measured in kB, but we want MB
+                result['memory'] += (int(line.split(':')[1]) / 4) / 1000
+
+        return result
+
+def main(argv):
+    results = Results()
+    
+    cpu_count = multiprocessing.cpu_count()
+    for cores in range(1, cpu_count + 1):
+        # LBP
+        if cores == 1:
+            print("Benchmarking LBP...")
+            process = subprocess.Popen(['/usr/bin/time', '-v', 'python2', 'main.py', 'images/1.jpeg', 'lbp'], stderr=subprocess.PIPE)
+            results.append('lbp', cores, process.stderr)
+
+        # Multiprocessing LBP
+        print("Benchmarking multiprocessing LBP with {} cores...".format(cores))
+        process = subprocess.Popen(['/usr/bin/time', '-v', 'python2', 'main.py', 'images/1.jpeg', 'multi-lbp', str(cores)], stderr=subprocess.PIPE)
+        results.append('multi-lbp', cores, process.stderr)
+
+        # Multiprocessing LBP
+        print("Benchmarking multiprocessing split LBP with {} cores...".format(cores))
+        process = subprocess.Popen(['/usr/bin/time', '-v', 'python2', 'main.py', 'images/1.jpeg', 'multi-split-lbp', str(cores)], stderr=subprocess.PIPE)
+        results.append('multi-split-lbp', cores, process.stderr)
+
+    print("Writing results to benchmark_data.json...")
+    with open('benchmark_data.json', 'w') as output:
+        json.dump(results.get(), output, indent=4, separators=(',', ': '))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- benchmark.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-stdbuf -o L `which time` -f "real\t%E\nuser\t%U\nsys\t%S\nmem\t%M/4 kB" $@ 2>&1
