71ee887d54de9df8f142967464a54a4004cfc32c
Tim van der Meij <timvandermeij@gmail.com>
2015-04-29 12:37:09 +0200

    Resolve inverted logic
    Does not really change the output images. There are various
    implementations possible, but this new one seems to be the most common.

--- algorithms/LBP.py
+++ algorithms/LBP.py
@@ -29,14 +29,14 @@ class LBP:
                 # clockwise, and use bit operations to efficiently update the feature vector
                 pixel = current_row[j]
                 pattern = 0
-                pattern = pattern | (1 << 0) if pixel > previous_row[j-1] else pattern
-                pattern = pattern | (1 << 1) if pixel > previous_row[j] else pattern
-                pattern = pattern | (1 << 2) if pixel > previous_row[j+1] else pattern
-                pattern = pattern | (1 << 3) if pixel > current_row[j+1] else pattern
-                pattern = pattern | (1 << 4) if pixel > next_row[j+1] else pattern
-                pattern = pattern | (1 << 5) if pixel > next_row[j] else pattern
-                pattern = pattern | (1 << 6) if pixel > next_row[j-1] else pattern
-                pattern = pattern | (1 << 7) if pixel > current_row[j-1] else pattern
+                pattern = pattern | (1 << 0) if pixel < previous_row[j-1] else pattern
+                pattern = pattern | (1 << 1) if pixel < previous_row[j] else pattern
+                pattern = pattern | (1 << 2) if pixel < previous_row[j+1] else pattern
+                pattern = pattern | (1 << 3) if pixel < current_row[j+1] else pattern
+                pattern = pattern | (1 << 4) if pixel < next_row[j+1] else pattern
+                pattern = pattern | (1 << 5) if pixel < next_row[j] else pattern
+                pattern = pattern | (1 << 6) if pixel < next_row[j-1] else pattern
+                pattern = pattern | (1 << 7) if pixel < current_row[j-1] else pattern
                 self.patterns.append(pattern)
 
     def _output(self):
